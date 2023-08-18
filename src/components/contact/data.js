5a551c6cc277b93062661709aa5d6c33531e1f95
Zohaib <zohaibsoomro100@gmail.com>
2023-08-18 20:15:44 +0500

    website completed

---
+++ src/components/contact/data.js
@@ -0,0 +1,54 @@
+import location from "../../Images/map.png";
+import mail from "../../Images/mail.png";
+import whatsApp from "../../Images/whatsApp.png";
+import linkedIn from "../../Images/linkedIn.png";
+import facebook from "../../Images/facebook.png";
+import instagram from "../../Images/Instagram_logo_2016.svg.webp";
+
+export const contactdata = [
+  {
+    id: 1,
+    title: "Location",
+    title2: "Karachi, Pakistan",
+    img: location,
+    locationhover:true,
+
+  },
+  {
+    id: 2,
+    title: "Mail",
+    title2: "zohaibsoomro0101@gmail.com",
+    img: mail,
+  },
+  {
+    id: 3,
+    title: "WhatsApp",
+    title2: "0348 2469101",
+    img: whatsApp,
+  },
+  {
+    id: 4,
+    title: "LinkedIn",
+    title2: "Muhammad Zohaib",
+    img: linkedIn,
+    link: "https://www.linkedin.com/in/muhammad-zohaib-b94280190/",
+    hover:true
+    
+  },
+  {
+    id: 5,
+    title: "Facebook",
+    title2: "Muhammad Zohaib Soomro",
+    img: facebook,
+    link: "https://www.facebook.com/dashinggzohaib.soomro/",
+    hover:true
+  },
+  {
+    id: 6,
+    title: "Instagram",
+    title2: "muhammadzohaibsoomroo",
+    img: instagram,
+    link: "https://www.instagram.com/muhammadzohaibsoomroo/",
+    hover:true
+  },
+];
