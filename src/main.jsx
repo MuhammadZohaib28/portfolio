d1357b98f5ea4518a8d3644cbe4d3ec7c967c240
Muhammad Zohaib <76484677+MuhammadZohaib28@users.noreply.github.com>
2023-09-08 00:55:43 +0500

    chnages made

--- src/main.jsx
+++ src/main.jsx
@@ -3,25 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { ThemeProvider } from "./context/ThemeContext.jsx";
-// import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Resume from "./pages/resume/Resume.jsx";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "resume",
-    element: <Resume />,
-  },
-]);
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider>
-      <RouterProvider router={router} />
+      <App />
     </ThemeProvider>
   </React.StrictMode>
 );
