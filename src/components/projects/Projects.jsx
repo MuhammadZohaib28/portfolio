e3d55b146c943894cf9aa2ec3221bf60b33224b8
Muhammad Zohaib <76484677+MuhammadZohaib28@users.noreply.github.com>
2025-07-02 01:52:14 +0500

    projects sorted by latest one should show first.

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -32,100 +32,102 @@ const Projects = () => {
           <h2>Each project is a unique piece of development 🧩</h2>
         </div>
 
-        {projectdata.map((item) => {
-          return (
-            <div className="bottom" key={item.id}>
-              <div
-                className={
-                  item.rowreverse == true
-                    ? "containerb containerbrow"
-                    : "containerb"
-                }
-              >
-                <div className={item.hover ? "leftb" : "leftbb"}>
-                  <img
-                    src={item.projectpicture}
-                    alt="projectpicture"
-                    loading="lazy"
-                  />
+        {[...projectdata]
+          .sort((a, b) => b.id - a.id)
+          .map((item) => {
+            return (
+              <div className="bottom" key={item.id}>
+                <div
+                  className={
+                    item.rowreverse == true
+                      ? "containerb containerbrow"
+                      : "containerb"
+                  }
+                >
+                  <div className={item.hover ? "leftb" : "leftbb"}>
+                    <img
+                      src={item.projectpicture}
+                      alt="projectpicture"
+                      loading="lazy"
+                    />
 
-                  {item.hover ? (
-                    <div className="overlay">
-                      <span>
-                        {touch
-                          ? "Tap here to see majic. 👌"
-                          : "Hover here to see majic. 👌"}
-                      </span>
-                    </div>
-                  ) : null}
-                </div>
-                <div className="rightb">
-                  <div className="topr">
-                    <h3>{item.projectname}</h3>
+                    {item.hover ? (
+                      <div className="overlay">
+                        <span>
+                          {touch
+                            ? "Tap here to see majic. 👌"
+                            : "Hover here to see majic. 👌"}
+                        </span>
+                      </div>
+                    ) : null}
                   </div>
-
-                  <div className="midb">
-                    <p>{item.projectdesc}</p>
-                    <div className="midbtn">
-                      <span>{item.projectlanguage1}</span>
-                      <span>{item.projectlanguage2}</span>
-                    </div>
-                  </div>
-
-                  <div className="bottomb">
-                    <div className="btns">
-                      <a
-                        href={item.live}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="rightbb"
-                      >
-                        {item.livedemo ? (
-                          <span>Live demo</span>
-                        ) : (
-                          <h3>Not Avaialble</h3>
-                        )}
-
-                        <img
-                          src={live}
-                          alt=""
-                          className="imglive"
-                          loading="lazy"
-                        />
-                      </a>
+                  <div className="rightb">
+                    <div className="topr">
+                      <h3>{item.projectname}</h3>
                     </div>
 
-                    {item.code ? (
-                      <div className="btnss">
+                    <div className="midb">
+                      <p>{item.projectdesc}</p>
+                      <div className="midbtn">
+                        <span>{item.projectlanguage1}</span>
+                        <span>{item.projectlanguage2}</span>
+                      </div>
+                    </div>
+
+                    <div className="bottomb">
+                      <div className="btns">
                         <a
-                          href={item.code}
+                          href={item.live}
                           target="_blank"
                           rel="noopener noreferrer"
                           className="rightbb"
                         >
-                          <span>Code</span>
-                          <img src={github} alt="" loading="lazy" />
+                          {item.livedemo ? (
+                            <span>Live demo</span>
+                          ) : (
+                            <h3>Not Avaialble</h3>
+                          )}
+
+                          <img
+                            src={live}
+                            alt=""
+                            className="imglive"
+                            loading="lazy"
+                          />
                         </a>
                       </div>
-                    ) : (
-                      <div className="btnss">
-                        <a
-                          href={item.code}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          className="rightbb"
-                        >
-                          <span>Private Repository</span>
-                          <img src={github} alt="" loading="lazy" />
-                        </a>
-                      </div>
-                    )}
+
+                      {item.code ? (
+                        <div className="btnss">
+                          <a
+                            href={item.code}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="rightbb"
+                          >
+                            <span>Code</span>
+                            <img src={github} alt="" loading="lazy" />
+                          </a>
+                        </div>
+                      ) : (
+                        <div className="btnss">
+                          <a
+                            href={item.code}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="rightbb"
+                          >
+                            <span>Private Repository</span>
+                            <img src={github} alt="" loading="lazy" />
+                          </a>
+                        </div>
+                      )}
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
       </div>
     </div>
   );
